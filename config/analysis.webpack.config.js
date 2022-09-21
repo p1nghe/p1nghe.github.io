dc779e701c9c04d14beb93cb2c04e5a6cdbecf50
nobo4y <ace@bugbank.cn>
2022-09-21 12:03:13 +0800

    index
    index

--- config/analysis.webpack.config.js
+++
@@ -1,6 +0,0 @@
-const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
-const config = require('./production.webpack.config');
-
-config.plugins.push(new BundleAnalyzerPlugin());
-
-module.exports = config;
